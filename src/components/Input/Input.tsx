e1ddb059c0f0c1dea5b2518a55d2996e38cb4b94
sergio santamaria fajardo <santamaria.fajardo@gmail.com>
2026-03-05 20:24:01 +0100

    work on inputs

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,22 +1,42 @@
-import { forwardRef } from "react";
+import { forwardRef, useState } from "react";
 import { InputProps } from "./Input.types";
 import "./Input.css";
+import { Icon } from "../../icons";
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ type = "text", disabled, onChange, ...props }, ref) => {
+  ({ type = "text", onChange,  ...props }, ref) => {
+    const [value, setValue] = useState("");
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(e.currentTarget.value);
+      onChange?.(e);
+    };
+    const iconsByType = {
+      'color': 'palette',
+      'date': 'calendar-dots',
+      'search': 'magnifying-glass',
+      'text': '',
+      'number': '',
+      'password': '',
+      'email': '',
+      'tel': ''
+    }
     return (
-      <div
-        className={`modern-input ${disabled ? "modern-input__disabled" : ""}`}
-        data-input-type={type}
-      >
-        <input
-          ref={ref}
-          type={type}
-          disabled={disabled}
-          onChange={onChange}
-          {...props}
-        />
-        <div className="modern-input__display" />
+      <div className="modern-input" data-input-type={type}>
+          <div className="modern-input__input-mask"> 
+             <input
+              ref={ref}
+              aria-label="input-type-aria-label"
+              className="modern-input__inputbox"
+              type={type}
+              onChange={handleChange}
+              {...props}
+            />
+            { ['search', 'color', 'date'].includes(type)? 
+              <div className="modern-input__input-icon">
+                <Icon name={iconsByType[type]} variant="light"/>
+            </div> : <></>
+            }
+          </div>
       </div>
     );
   }
--- src/components/Input/Input.types.tsx
+++ src/components/Input/Input.types.tsx
@@ -1,5 +1,6 @@
 import { InputHTMLAttributes } from "react";
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-    type?: 'text' | 'number' | 'password' | 'email' | 'tel' | 'search' | 'date' | 'color';
+    type?: 'text' | 'number' | 'password' | 'email' | 'tel' | 'search' | 'date' | 'color',
+    onChange?: (value: React.ChangeEvent<HTMLInputElement>) => void
 }
